608a36ce8fc7977909a199b55f6eb77398f39151
sumishin <sumishin@example.com>
2016-08-27 14:58:13 +0900

    initial settings

---
+++ built/app.js
@@ -0,0 +1,7 @@
+/// <reference path="../typings/index.d.ts" />
+jQuery(document).ready(() => {
+    $("#startbutton").on("click", () => {
+        window.alert("test");
+    });
+});
+//# sourceMappingURL=app.js.map
